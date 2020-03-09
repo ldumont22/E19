1df9d9b127998c156f51551e01ea9e558fedcef9
ldumont22 <ldumont@student.s19.be>
2020-03-09 17:38:32 +0100

    Day 2

--- printf/ft_flags_aux.c
+++ printf/ft_flags_aux.c
@@ -1,38 +1,33 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_printf.c                                        :+:      :+:    :+:   */
+/*   ft_flags_aux.c                                     :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: ldumont <ldumont@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2020/03/02 13:59:13 by ldumont           #+#    #+#             */
+/*   Created: 2020/03/09 12:36:37 by ldumont           #+#    #+#             */
 /*   Updated: 2020/03/09 17:38:31 by ldumont          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-# include <stdarg.h>
-# include <stdio.h>
+#include "../include/ft_printf.h"
 
-int	ft_printf(const char *format,...)
+int		ft_init_flags(t_flags *flags)
 {
-	int nb;
-	va_list ap;
-	t_flags flags; // creation de la structure des flags
-	
-	ft_init_flags(&flags) // envoi des flags pour init 0
-	
-	 
-
-	va_start(ap, format);
-	
-	
-	va_end(ap);
-}d
-
-
-int		main()
-{
-	//ft_print("|%.03s|", NULL)
-	printf("|%.03s|", "32");
-	return (0);
+	flags->minus = 0; // Remplir les espaces par des -
+	flags->zpad = 0; // Remplir les espaces par 0
+	flags->min_w = 0; // largeur minimale
+	flags->conv = 0; // 
+	flags->negatif = 0; // Chiffre negatif
+	flags->prec = -1; // precision
 }
+
+void	get_minus_zpad(const char *str, t_flags *flags)
+{
+	if (str[i] == '-')
+		flags-> = 1;
+	else if (str[i] == '0')
+		flags_zpad = 1;
+}
+
+void	get_min_w(const char *str, t_flags *flags)
